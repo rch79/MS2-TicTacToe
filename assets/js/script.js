28722b47d888fe078de5e4caa223ff0ee2f1c56e
rafachavz <60248744+rafachavz@users.noreply.github.com>
2021-08-15 16:41:14 +0100

    Add board position event handlers and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,56 +41,241 @@ let gamePageHTML = `
     </section>
     <section id="board-grid">
         <div class="game-board">
-            <div class="box" id="position-0"></div>
-            <div class="box" id="position-1"></div>
-            <div class="box" id="position-2"></div>
-            <div class="box" id="position-3"></div>
-            <div class="box" id="position-4"></div>
-            <div class="box" id="position-5"></div>
-            <div class="box" id="position-6"></div>
-            <div class="box" id="position-7"></div>
-            <div class="box" id="position-8"></div>
+            <div class="box" id="position-0"> </div>
+            <div class="box" id="position-1"> </div>
+            <div class="box" id="position-2"> </div>
+            <div class="box" id="position-3"> </div>
+            <div class="box" id="position-4"> </div>
+            <div class="box" id="position-5"> </div>
+            <div class="box" id="position-6"> </div>
+            <div class="box" id="position-7"> </div>
+            <div class="box" id="position-8"> </div>
         </div> 
     </section>
     <script src="assets/js/script.js"></script>`;
 
 
 //--------------------------------------------------------Main variables--------------
-let player_1 = "";
-let player_2 = "";
+let player1Choice = "";
+let player2Choice = "";
+let currentChoice = "";
+let gameBoard = [" "," "," "," "," "," "," "," "," "];
 
 //--------------------------------------------------------Functions-------------------
 function clickButtonX() {
-	player_1 = "X";
-	player_2 = "O";
+	player1Choice = "X";
+	player2Choice = "O";
 
 	document.getElementById("buttonx_choice").innerHTML = "<h3>Player 1</h3>";
 	document.getElementById("buttono_choice").innerHTML = "<h3>Player 2</h3>";
 }
 
 function clickButtonO() {
-	player_1 = "O";
-	player_2 = "X";
+	player1Choice = "O";
+	player2Choice = "X";
 
 	document.getElementById("buttonx_choice").innerHTML = "<h3>Player 2</h3>";
 	document.getElementById("buttono_choice").innerHTML = "<h3>Player 1</h3>";
 }
 
-function generateGameHTML() {
-	if(player_1 == "" && player_2 == "" ) {
+function setCurrentChoice() {
+	if(currentChoice == "X") {
+		currentChoice = "O";
+	} else {
+		currentChoice = "X";
+	}
+}
+	
+function startGame() {
+	if(player1Choice == "" && player2Choice == "" ) {
 		window.alert("Please select an option first");
 	} else {
 			document.body.innerHTML = gamePageHTML;
+			
+			currentChoice = player1Choice;
+			getBoardValues();
+
+			initializeBoardHandlers();
+	}
+}
+
+function getBoardValues() {
+	for(let i = 0; i < gameBoard.length; i++) {
+		gameBoard[i] = document.getElementById("position-" +i).innerHTML;
+	}
+	console.log(gameBoard[0]);
+}
+
+function playerTurn0() {
+	if (document.getElementById("position-0").innerHTML != " ") {
+		window.alert("Invalid selection!")
+	} else {
+		console.log("hello!");
+		console.log("this is " + gameBoard[0])
+		document.getElementById("position-0").innerHTML = currentChoice;
+		isVictory();
+		setCurrentChoice();
+	}
+}
+
+function playerTurn1() {
+	if (document.getElementById("position-1").innerHTML != " ") {
+		window.alert("Invalid selection!")
+	} else {
+		console.log("hello!");
+		console.log("this is " + gameBoard[1])
+		document.getElementById("position-1").innerHTML = currentChoice;
+		isVictory();
+		setCurrentChoice();
+	}
+}
+
+function playerTurn2() {
+	if (document.getElementById("position-2").innerHTML != " ") {
+		window.alert("Invalid selection!")
+	} else {
+		console.log("hello!");
+		console.log("this is " + gameBoard[2])
+		document.getElementById("position-2").innerHTML = currentChoice;
+		isVictory();
+		setCurrentChoice();
+	}
+}
+
+function playerTurn3() {
+	if (document.getElementById("position-3").innerHTML != " ") {
+		window.alert("Invalid selection!")
+	} else {
+		console.log("hello!");
+		console.log("this is " + gameBoard[3])
+		document.getElementById("position-3").innerHTML = currentChoice;
+		isVictory();
+		setCurrentChoice();
+	}
+}
+
+function playerTurn4() {
+	if (document.getElementById("position-4").innerHTML != " ") {
+		window.alert("Invalid selection!")
+	} else {
+		console.log("hello!");
+		console.log("this is " + gameBoard[4])
+		document.getElementById("position-4").innerHTML = currentChoice;
+		isVictory();
+		setCurrentChoice();
+	}
+}
+
+function playerTurn5() {
+	if (document.getElementById("position-5").innerHTML != " ") {
+		window.alert("Invalid selection!")
+	} else {
+		console.log("hello!");
+		console.log("this is " + gameBoard[5])
+		document.getElementById("position-5").innerHTML = currentChoice;
+		isVictory();
+		setCurrentChoice();
+	}
+}
+
+function playerTurn6() {
+	if (document.getElementById("position-6").innerHTML != " ") {
+		window.alert("Invalid selection!")
+	} else {
+		console.log("hello!");
+		console.log("this is " + gameBoard[6])
+		document.getElementById("position-6").innerHTML = currentChoice;
+		isVictory();
+		setCurrentChoice();
+	}
+}
+
+function playerTurn7() {
+	if (document.getElementById("position-7").innerHTML != " ") {
+		window.alert("Invalid selection!")
+	} else {
+		console.log("hello!");
+		console.log("this is " + gameBoard[7])
+		document.getElementById("position-7").innerHTML = currentChoice;
+		isVictory();
+		setCurrentChoice();
+	}
+}
+
+function playerTurn8() {
+	if (document.getElementById("position-8").innerHTML != " ") {
+		window.alert("Invalid selection!")
+	} else {
+		console.log("hello!");
+		console.log("this is " + gameBoard[8])
+		document.getElementById("position-8").innerHTML = currentChoice;
+		isVictory();
+		setCurrentChoice();
 	}
 }
 
 
-//--------------------------------------------------------Event Listeners-------------
-let buttonX = document.getElementById("player-selection-X");
-buttonX.addEventListener('click', clickButtonX);
+function initializeBoardHandlers() {
+	let handleBoardClick0 = document.getElementById("position-0");
+	handleBoardClick0.addEventListener('click', playerTurn0);
 
-let buttonO = document.getElementById("player-selection-O");
-buttonO.addEventListener('click', clickButtonO);
+	let handleBoardClick1 = document.getElementById("position-1");
+	handleBoardClick1.addEventListener('click', playerTurn1);
 
-let startGame = document.getElementById("start-game-button");
-startGame.addEventListener('click', generateGameHTML);
+	let handleBoardClick2 = document.getElementById("position-2");
+	handleBoardClick2.addEventListener('click', playerTurn2);
+
+	let handleBoardClick3 = document.getElementById("position-3");
+	handleBoardClick3.addEventListener('click', playerTurn3);
+
+	let handleBoardClick4 = document.getElementById("position-4");
+	handleBoardClick4.addEventListener('click', playerTurn4);
+
+	let handleBoardClick5 = document.getElementById("position-5");
+	handleBoardClick5.addEventListener('click', playerTurn5);
+
+	let handleBoardClick6 = document.getElementById("position-6");
+	handleBoardClick6.addEventListener('click', playerTurn6);
+
+	let handleBoardClick7 = document.getElementById("position-7");
+	handleBoardClick7.addEventListener('click', playerTurn7);
+
+	let handleBoardClick8 = document.getElementById("position-8");
+	handleBoardClick8.addEventListener('click', playerTurn8);
+}
+
+function isVictory() {
+	getBoardValues();
+	let victory = false;
+	let winner = ""
+
+	if(gameBoard[0] != " " && gameBoard[0] == gameBoard[1] && gameBoard[0] == gameBoard[2]) {
+		winner = gameBoard[0];
+		victory = true;
+			
+	}
+
+	if(victory) {
+		if(player1Choice == winner) {
+			window.alert("Player 1 Wins!!!");
+		} else {
+			window.alert("Player 2 Wins!!!!");
+		}
+		return true;	
+	} else {
+		return false;
+	}
+}
+
+//--------------------------------------------------------Event Handlers-------------
+let handleClickButtonX = document.getElementById("player-selection-X");
+handleClickButtonX.addEventListener('click', clickButtonX);
+
+let handleClickButtonO = document.getElementById("player-selection-O");
+handleClickButtonO.addEventListener('click', clickButtonO);
+
+let handleStartGame = document.getElementById("start-game-button");
+handleStartGame.addEventListener('click', startGame);
+
+
+
