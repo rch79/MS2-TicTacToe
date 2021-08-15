7132801919639ab2471ccda1bc17270b411d0362
rafachavz <60248744+rafachavz@users.noreply.github.com>
2021-08-15 02:46:34 +0100

    Add player selection button scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
 console.log("script test");
+
+let player_1 = "";
+let player_2 = "";
+
+function clickButtonX() {
+	console.log("button clicked!!!!!!")
+	player_1 = "X";
+	player_2 = "O";
+
+	document.getElementById("buttonx_choice").innerHTML = "<h3>Player 1</h3>"
+	document.getElementById("buttono_choice").innerHTML = "<h3>Player 2</h3>"
+
+
+	console.log(player_1);
+	console.log(player_2);
+}
+
+function clickButtonO() {
+	player_1 = "O";
+	player_2 = "X";
+
+	document.getElementById("buttonx_choice").innerHTML = "<h3>Player 2</h3>"
+	document.getElementById("buttono_choice").innerHTML = "<h3>Player 1</h3>"
+
+	console.log(player_1);
+	console.log(player_2);
+}
+
+console.log(player_1);
+
+let buttonX = document.getElementById("player-selection-X");
+buttonX.addEventListener('click', clickButtonX);
+
+let buttonO = document.getElementById("player-selection-O");
+buttonO.addEventListener('click', clickButtonO);
