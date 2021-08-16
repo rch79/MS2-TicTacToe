9300d2ebc01798e7554cd919bf0592c777f7c61f
rafachavz <60248744+rafachavz@users.noreply.github.com>
2021-08-17 00:16:20 +0100

    Add player scores and cleanup functions to remove debug and redundant code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//--------------------------------------------------------Start page and game page HTML code-----
+//----------------------------------------------------------------------------------------Start page and game page HTML code-----
 
 let startPageHTML = `
     <header>
@@ -34,13 +34,18 @@ let gamePageHTML = `
             <h1>Tic-Tac-Toe</h1>
         </div>
     </header>
-    <section id="player-scores-div">
+    <section id="player-scores">
         <div id="player1-score">
             <h2>Player 1 - Score: 0</h2>
         </div>
         <div id ="player2-score">
         	<h2>Player 2 - Score: 0</h2>
         </div>
+    </section>
+    <section>
+    	<div class="alert-message">
+        	<h3></h3>
+        </div>        
     </section>
     <section id="board-grid">
         <div class="game-board">
@@ -57,18 +62,24 @@ let gamePageHTML = `
     </section>
     <script src="assets/js/script.js"></script>`;
 
-
-//--------------------------------------------------------Main variables--------------
+//---------------------------------------------------------------------------------------------------Main variables--------------
 let player1Choice = "";
 let player2Choice = "";
 let currentChoice = "";
 let gameBoard = [" "," "," "," "," "," "," "," "," "];
+let player1Score = 0;
+let player2Score = 0;
 
-//--------------------------------------------------------Functions-------------------
+//---------------------------------------------------------------------------------------------------Functions-------------------
 
-function alertMessage(index, message) {
-	document.getElementsByClassName("alert-message")[index].innerHTML = "<h3>" + message + "</h3>";
-	setTimeout(() => { document.getElementsByClassName("alert-message")[index].innerHTML = ""; }, 700);
+function alertMessage(message) {
+	document.getElementsByClassName("alert-message")[0].innerHTML = "<h3>" + message + "</h3>";
+	setTimeout(() => { document.getElementsByClassName("alert-message")[0].innerHTML = ""; }, 700);
+}
+
+function setPlayerScores() {
+	document.getElementById("player1-score").innerHTML = "<h2>Player 1- Score: " + player1Score + "</h2>";
+	document.getElementById("player2-score").innerHTML = "<h2>Player 2- Score: " + player2Score + "</h2>";
 }
 function clickButtonX() {
 	player1Choice = "X";
@@ -100,13 +111,11 @@ function setCurrentChoice() {
 	
 function startGame() {
 	if(player1Choice == "" && player2Choice == "" ) {
-		alertMessage(0,"Please select an option first!!!");
+		alertMessage("Please select an option first!!!");
 	} else {
 			document.body.innerHTML = gamePageHTML;
-			
 			currentChoice = player1Choice;
-			getBoardValues();
-
+			setPlayerScores();
 			initializeBoardHandlers();
 	}
 }
@@ -120,109 +129,82 @@ function getBoardValues() {
 
 function playerTurn0() {
 	if (document.getElementById("position-0").innerHTML != " ") {
-		window.alert("Invalid selection!")
+		alertMessage("Invalid Selection!!!");
 	} else {
-		console.log("hello!");
-		console.log("this is " + gameBoard[0])
 		document.getElementById("position-0").innerHTML = currentChoice;
 		isVictory();
-		setCurrentChoice();
 	}
 }
 
 function playerTurn1() {
 	if (document.getElementById("position-1").innerHTML != " ") {
-		window.alert("Invalid selection!")
+		alertMessage("Invalid Selection!!!");
 	} else {
-		console.log("hello!");
-		console.log("this is " + gameBoard[1])
 		document.getElementById("position-1").innerHTML = currentChoice;
 		isVictory();
-		setCurrentChoice();
 	}
 }
 
 function playerTurn2() {
 	if (document.getElementById("position-2").innerHTML != " ") {
-		window.alert("Invalid selection!")
+		alertMessage("Invalid Selection!!!");
 	} else {
-		console.log("hello!");
-		console.log("this is " + gameBoard[2])
 		document.getElementById("position-2").innerHTML = currentChoice;
 		isVictory();
-		setCurrentChoice();
 	}
 }
 
 function playerTurn3() {
 	if (document.getElementById("position-3").innerHTML != " ") {
-		window.alert("Invalid selection!")
+		alertMessage("Invalid Selection!!!");
 	} else {
-		console.log("hello!");
-		console.log("this is " + gameBoard[3])
 		document.getElementById("position-3").innerHTML = currentChoice;
 		isVictory();
-		setCurrentChoice();
 	}
 }
 
 function playerTurn4() {
 	if (document.getElementById("position-4").innerHTML != " ") {
-		window.alert("Invalid selection!")
+		alertMessage("Invalid Selection!!!");
 	} else {
-		console.log("hello!");
-		console.log("this is " + gameBoard[4])
 		document.getElementById("position-4").innerHTML = currentChoice;
 		isVictory();
-		setCurrentChoice();
 	}
 }
 
 function playerTurn5() {
 	if (document.getElementById("position-5").innerHTML != " ") {
-		window.alert("Invalid selection!")
+		alertMessage("Invalid Selection!!!");
 	} else {
-		console.log("hello!");
-		console.log("this is " + gameBoard[5])
 		document.getElementById("position-5").innerHTML = currentChoice;
 		isVictory();
-		setCurrentChoice();
 	}
 }
 
 function playerTurn6() {
 	if (document.getElementById("position-6").innerHTML != " ") {
-		window.alert("Invalid selection!")
+		alertMessage("Invalid Selection!!!");
 	} else {
-		console.log("hello!");
-		console.log("this is " + gameBoard[6])
 		document.getElementById("position-6").innerHTML = currentChoice;
 		isVictory();
-		setCurrentChoice();
 	}
 }
 
 function playerTurn7() {
 	if (document.getElementById("position-7").innerHTML != " ") {
-		window.alert("Invalid selection!")
+		alertMessage("Invalid Selection!!!");
 	} else {
-		console.log("hello!");
-		console.log("this is " + gameBoard[7])
 		document.getElementById("position-7").innerHTML = currentChoice;
 		isVictory();
-		setCurrentChoice();
 	}
 }
 
 function playerTurn8() {
 	if (document.getElementById("position-8").innerHTML != " ") {
-		window.alert("Invalid selection!")
+		alertMessage("Invalid Selection!!!");
 	} else {
-		console.log("hello!");
-		console.log("this is " + gameBoard[8])
 		document.getElementById("position-8").innerHTML = currentChoice;
 		isVictory();
-		setCurrentChoice();
 	}
 }
 
@@ -276,20 +258,25 @@ function isVictory() {
 		window.alert("Everybody loses");
 		victory = false;
 	} else {
+		setCurrentChoice();
 		return false;
 	}
 
 	if(victory) {
 		if(player1Choice == winner) {
-			window.alert("Player 1 Wins!!!");
+			alertMessage("Player 1 Wins!!!");
+			player1Score += 1;
+			startGame();
 		} else {
-			window.alert("Player 2 Wins!!!!");
+			alertMessage("Player 2 Wins!!!!");
+			player2Score += 1;
+			startGame();
 		}
 		return true;	
 	}
 }
 
-//--------------------------------------------------------Event Handlers-------------
+//-------------------------------------------------------------------------------------------------------Event Handlers-------------
 let handleClickButtonX = document.getElementById("player-selection-button-x");
 handleClickButtonX.addEventListener('click', clickButtonX);
 
